f689ed493d12347752d486de8503a1f0972dc4fa
AhmedElkrrai <ahmedemadelkrrai@gmail.com>
2025-04-08 19:13:45 +0200

    enableStickyImmersiveMode

--- app/src/main/java/com/example/altabib/MainActivity.kt
+++ app/src/main/java/com/example/altabib/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.altabib
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,12 +9,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.altabib.featuers.user.domain.entities.UserType
 import com.example.altabib.featuers.user.domain.usecases.GetUserUseCase
-import com.example.altabib.navigation.utils.LocalNavController
 import com.example.altabib.navigation.graph.RegistrationNavGraph
 import com.example.altabib.navigation.screen.Screen
+import com.example.altabib.navigation.utils.LocalNavController
 import com.example.altabib.ui.theme.AlTabibTheme
+import com.example.altabib.utils.enableStickyImmersiveMode
 import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +22,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        hideSystemBars()
+        enableStickyImmersiveMode()
         setContent {
             AlTabibTheme {
                 val rootNavController = rememberNavController()
@@ -47,23 +43,6 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
-        }
-    }
-
-    private fun hideSystemBars() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.let { controller ->
-                controller.hide(WindowInsets.Type.systemBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    )
         }
     }
 }
--- app/src/main/java/com/example/altabib/featuers/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/altabib/featuers/home/presentation/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.altabib.featuers.home.presentation
 
+import androidx.activity.compose.LocalActivity
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.rememberNavController
 import com.example.altabib.featuers.user.domain.entities.UserType
 import com.example.altabib.featuers.user.domain.usecases.GetUserUseCase
@@ -9,9 +11,16 @@ import com.example.altabib.navigation.bar.DoctorBottomBar
 import com.example.altabib.navigation.bar.PatientBottomBar
 import com.example.altabib.navigation.graph.DoctorNavGraph
 import com.example.altabib.navigation.graph.PatientNavGraph
+import com.example.altabib.utils.enableStickyImmersiveMode
 
 @Composable
 fun HomeScreen(getUser: GetUserUseCase) {
+    val activity = LocalActivity.current
+
+    LaunchedEffect(Unit) {
+        activity?.enableStickyImmersiveMode()
+    }
+
     val navController = rememberNavController()
     val userType = getUser.invoke()?.type ?: UserType.Patient
 
